c3ef8cc8294b408eb520067aa91f6c44349fa31c
Silvena Chan <silvena.chan@vimeo.com>
2023-07-19 15:43:46 -0400

    Only the most wrong styling

--- node_helpers.go
+++ node_helpers.go
@@ -6,7 +6,14 @@ import (
 )
 
 func createElemAtom(d atom.Atom) *html.Node {
-	return &html.Node{Type: html.ElementNode, DataAtom: d, Data: d.String()}
+	n := &html.Node{Type: html.ElementNode, DataAtom: d, Data: d.String()}
+	if n.DataAtom == atom.Table {
+		n.Attr = append(n.Attr, html.Attribute{
+			Key: "style",
+			Val: "border: 1px solid; min-width: 100px",
+		})
+	}
+	return n
 }
 
 func textNode(d string) *html.Node {
